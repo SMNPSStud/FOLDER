884f58012ec86c578b37e4ad877643e4ad9e5e87
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-06 11:52:04 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.penup()
+turtle.setposition(-200,200)
+turtle.pendown()
+turtle.pencolor("red")
+turtle.circle(100)
+
+turtle.penup()
+turtle.setposition(-200,-200)
+turtle.pendown()
+turtle.pencolor("green")
+turtle.circle(200)
